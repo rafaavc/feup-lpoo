2613710cf586afaf67b42f86718e31c0ee85fd8c
rafaavc <rafavc2508@gmail.com>
2020-03-26 19:47:30 +0000

    [TP5] done

---
+++ TP5/src/main/java/FerengiClient.java
@@ -0,0 +1,5 @@
+public class FerengiClient extends AlienClient {
+    protected OrderingStrategy createOrderingStrategy() {
+        return new SmartStrategy();
+    }
+}
---
+++ TP5/src/main/java/RomulanClient.java
@@ -0,0 +1,5 @@
+public class RomulanClient extends AlienClient {
+    protected OrderingStrategy createOrderingStrategy() {
+        return new ImpatientStrategy();
+    }
+}
