0158456418fb4c2e0841a0533d0e7534dcb2ec79
rafaavc <rafavc2508@gmail.com>
2020-04-02 18:43:33 +0100

    [TP6] ex5 done i think

--- TP6/src/main/java/com/aor/refactoring/example5/Turtle.java
+++ TP6/src/main/java/com/aor/refactoring/example5/Turtle.java
@@ -1,8 +1,7 @@
 package com.aor.refactoring.example5;
 
 public class Turtle {
-    private int row;
-    private int column;
+    private int row, column;
     private char direction;
 
     public Turtle(int row, int column, char direction) {
@@ -23,22 +22,34 @@ public class Turtle {
         return direction;
     }
 
+    private void rotateLeft() {
+        if (direction == 'N') direction = 'W';
+        else if (direction == 'W') direction = 'S';
+        else if (direction == 'S') direction = 'E';
+        else if (direction == 'E') direction = 'N';
+    }
+
+    private void rotateRight() {
+        if (direction == 'N') direction = 'E';
+        else if (direction == 'E') direction = 'S';
+        else if (direction == 'S') direction = 'W';
+        else if (direction == 'W') direction = 'N';
+    }
+
+    private void moveForward() {
+        if (direction == 'N') row--;
+        else if (direction == 'S') row++;
+        else if (direction == 'W') column--;
+        else if (direction == 'E') column++;
+    }
+
     public void execute(char command) {
         if (command == 'L') { // ROTATE LEFT
-            if (direction == 'N') direction = 'W';
-            else if (direction == 'W') direction = 'S';
-            else if (direction == 'S') direction = 'E';
-            else if (direction == 'E') direction = 'N';
+            rotateLeft();
         } else if (command == 'R') { // ROTATE RIGHT
-            if (direction == 'N') direction = 'E';
-            else if (direction == 'E') direction = 'S';
-            else if (direction == 'S') direction = 'W';
-            else if (direction == 'W') direction = 'N';
+            rotateRight();
         } else if (command == 'F'){ // MOVE FORWARD
-            if (direction == 'N') row--;
-            if (direction == 'S') row++;
-            if (direction == 'W') column--;
-            if (direction == 'E') column++;
+            moveForward();
         }
     }
 }
